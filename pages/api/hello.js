05336164818e04098e1b8522852f17d7a4bb099a
Joey Anuff <joeyanuff@gmail.com>
2021-05-16 12:48:59 -0700

    token test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,19 @@
-const { createClient } = require("@astrajs/collections")
+const { createClient } = require("@astrajs/collections");
 
 export default async (req, res) => {
   const region = process.env.ASTRA_DB_REGION
-  const name = req.query.name ?? "World" 
-
+  const name = req.query.name ?? "World"
+  
+  // create an Astra client  
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+  });
+  
   res.status(200).json({ 
-    body: `Hello ${name}. Region is ${region}.` 
-  })
+    body: `Hello ${name}. 
+           Region: ${region}.
+           Token: ${astraClient.restClient.applicationToken}.` 
+          })
 }
