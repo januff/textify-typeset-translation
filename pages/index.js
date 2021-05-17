661fbb7e89ff4ade7bf03feaa6cd43f6ed813bb6
Joey Anuff <joeyanuff@gmail.com>
2021-05-16 17:25:55 -0700

    Link removed

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,11 @@ import LastFive from "../components/LastFive"
 import PageInfo from "../components/PageInfo"
 import Header from "../components/Header"
 import Translation from "../components/Translation"
-import Head from "next/head";
-import Link from "next/link";
 
 export default function Home() {
   const [page, setPage] = useState({ message: null })
   return (
     <div className="home-container">
-      <Head>
-        <Link
-          rel="preload"
-          href="/fonts/ReadyforAnythingBB-Regular.ttf"
-          as="font"
-          crossOrigin=""
-        />
-        <Link
-          rel="preload"
-          href="/fonts/ReadyforAnythingBB-Bold.ttf"
-          as="font"
-          crossOrigin=""
-        />
-      </Head>
       <div className="home-grid">
         <Header />
         <Translation 
