20bcbb722e93d9c769ccb70910d0000d62030e1d
Joey Anuff <joeyanuff@gmail.com>
2021-05-16 17:38:29 -0700

    _document preload

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/ReadyforAnythingBB-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/ReadyforAnythingBB-Bold.ttf"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
+
+
