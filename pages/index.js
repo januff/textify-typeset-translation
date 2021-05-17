fea376d92a27bdd669f942ea5dda05685cc7db61
Joey Anuff <joeyanuff@gmail.com>
2021-05-16 17:19:44 -0700

    preload font fix

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,27 @@ import LastFive from "../components/LastFive"
 import PageInfo from "../components/PageInfo"
 import Header from "../components/Header"
 import Translation from "../components/Translation"
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   const [page, setPage] = useState({ message: null })
   return (
     <div className="home-container">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/ReadyforAnythingBB-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/ReadyforAnythingBB-Bold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <div className="home-grid">
         <Header />
         <Translation 
